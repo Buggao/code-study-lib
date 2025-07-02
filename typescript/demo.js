2f8f84c517f5c4723a1e1dde2ba8083ea95e8ef2
Tone <gotton@foxmail.com>
2025-07-02 20:17:29 +0800

    一些提交

---
+++ typescript/demo.js
@@ -0,0 +1,15 @@
+function demo1() {
+    console.log("hello world");
+}
+
+const demo2 = function(){
+    console.log("demo2 hello world");
+} 
+
+const demo3 = () => {
+    console.log("demo3 hello world");
+}
+
+demo1();
+demo2();
+demo3();    
