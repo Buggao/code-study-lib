aac65258c8e9cb9102af68ac07a7dac498271bd8
Tone <gotton@foxmail.com>
2025-11-30 20:59:13 +0800

    refactor(typescript): 清理并重构类型相关代码
    删除无用的示例文件和旧代码
    简化index.ts中的类实现
    添加类型工具文档说明

--- typescript/index.ts
+++ typescript/index.ts
@@ -1,48 +1,14 @@
-abstract class Chess {
-  x:number = 0;
-  y:number = 0;
-  name:string = ""
-  Move(targetX:number, targetY:number) {
-    console.log("1、检查移动的位置是否合理");
-    console.log("2、检查位置上是否有棋子");
-    this.rule(targetX, targetY);
+class User {
+  constructor(public name: string, public age: number) {
   }
-  protected abstract rule(targetX:number, targetY:number):boolean 
-}
-
-class Pawn extends Chess {
-  rule(targetX:number, targetY:number):boolean {
-    console.log("兵的移动规则");
-    return true;
-  }
-} 
-
-class Knight extends Chess {
-  rule(targetX:number, targetY:number):boolean {
-    console.log("马的移动规则");
-    return true;
+  sayName() {
+    console.log(this.name);
   }
 }
 
-interface stringType {
-  [key:string]: string
-}
+function printUserProp(user: User, prop: keyof User) {
+  console.log(user[prop]);
+};
 
-interface numberType {
-  [key: number]: string | number
-}
-
-let myNumberType: numberType = {
-  10: "123",
-  0: "34",
-  12: 30
-}
-
-let testArray: numberType = ["name", "value"]
-
-console.log("testArray", testArray);
-
-let myStringType: stringType = {
-  name: "1",
-  value: "123"
-}
+const user = new User("张三", 18);
+printUserProp(user, "sayName");
--- typescript/pattern.ts
+++
@@ -1,5 +0,0 @@
-function foo(value: number | string) {
-    if(isString(value)) {
-        console.log(value.toUpperCase());
-    }
-}
